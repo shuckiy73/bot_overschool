7564832522a8e811b902da7ba5056d02ab92f826
Dmitry Shchutsky <121363089+shuckiy73@users.noreply.github.com>
2024-04-04 20:49:27 +0300

    Update tg_bot.py

--- lesson4/tg_bot.py
+++ lesson4/tg_bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 from postgres_client import save_phone_to_db
 
-bot = telebot.TeleBot("7055950109:AAGwHKJyfBYUChynWRweTRAF9AK9xUAPH30", parse_mode='HTML')
+bot = telebot.TeleBot("your token", parse_mode='HTML')
 
 
 @bot.message_handler(commands=['start'])
