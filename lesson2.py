14e72e1cb527118e3a2e5e6a4931699d791d6822
Dmitry Shchutsky <121363089+shuckiy73@users.noreply.github.com>
2024-04-04 20:46:09 +0300

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("7055950109:AAGwHKJyfBYUChynWRweTRAF9AK9xUAPH30", parse_mode='HTML')
+bot = telebot.TeleBot("your token", parse_mode='HTML')
 
 
 @bot.message_handler(commands=['start'])
